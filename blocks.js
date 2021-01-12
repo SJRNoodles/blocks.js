7582d0315aeeeeaafe0bf584ab3471be168534f8
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-01-12 17:53:32 -0600

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,55 @@
+// important variables
+var code = "";
+var variable = "";
+var codeview = "";
+var inny = "";
+var evalvar = "";
+var evalname = "";
+
+// sample variable
+var hello = "hello world";
+
+// settings
+var blockstarget = 'bjs_view'; // change this to a div id
+
+
+// custom block functions
+function log(in_){
+  alert(in_)
+}
+
+//blocks.js functions
+
+function value_set(){
+  var user_string_name = document.getElementById
+  ('bjsinput_name').value;
+  variable = document.getElementById('bjsinput_value').value;
+  evalvar = "var " + user_string_name + " = " + variable + ";";
+  console.log(evalvar);
+  addcode(evalvar,'var');
+  
+}
+
+function inner_set(varia,div){
+  evalname = document.getElementById("bjsinner").value;
+  console.log(evalname);
+  inny = varia;
+  console.log(div);
+  
+  document.getElementById("bjsinny_log").innerHTML = " <button onclick='addcode(" + '"log(' + evalname + ');","alert"' + ");'>logg</button>";
+  console.log(document.getElementById("bjsinny_log").innerHTML);
+ 
+}
+
+function addcode(input,type){
+var viewin = "<div class='blocksjs' id='blocks_" + type + "'>" + input + "</div>";
+code = code + input;
+
+codeview = codeview + viewin;
+document.getElementById(blockstarget).innerHTML = codeview;
+console.log(code);
+}
+
+function run(){
+  eval(code);
+}
